d2a96e0deac8b69c29941c82b72417298e43de1b
johnivann <magtotojohnivan@gmail.com>
2024-01-15 13:14:01 +0800

    added controller methods, model, and migrations

---
+++ app/Http/Controllers/SongController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Song;
+
+class SongController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(['message' => 'connected.']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+            'title' => 'required|min:3|max:255',
+            'artist' => 'required|min:3|max:255',
+            'album' => 'required|min:3|max:255',
+        ]);
+
+        Song::create($request->all());
+
+        return response()->json(['message', 'successfully added']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Song $song)
+    {
+        return $song;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Song $song)
+    {
+        request()->validate([
+            'title' => 'required|min:3|max:255',
+            'artist' => 'required|min:3|max:255',
+            'album' => 'required|min:3|max:255',
+        ]);
+
+        $song->update($request->all());
+
+        return response()->json(['message', 'successfully updated song.']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Song $song)
+    {
+        $song->delete();
+
+        return response()->json(['message', 'successfully deleted song.']);
+    }
+}

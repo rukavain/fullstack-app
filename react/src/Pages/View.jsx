f2057227d84189a15e654f914101021941e673dc
johnivann <magtotojohnivan@gmail.com>
2024-01-16 21:01:39 +0800

    added a delete button on list of songs

--- react/src/Pages/View.jsx
+++ react/src/Pages/View.jsx
@@ -6,6 +6,8 @@ import Button from "../components/Button.jsx";
 
 const View = () => {
     const [songs, setSongs] = useState([]);
+    const [delMessage, setDelMessage] = useState("");
+    const [isHidden, setIsHidden] = useState(false);
 
     useEffect(() => {
         axios
@@ -14,8 +16,30 @@ const View = () => {
             .catch((error) => console.error("Error fetching data", error));
     }, []);
 
+    const deleteSong = (songId) => {
+        axios
+            .delete(`http://localhost:8000/api/songs/${songId}`)
+            .then((response) => {
+                console.log(response.data);
+                setDelMessage("Song Deleted Successfully.");
+                setSongs(songs.map((song) => song.id !== songId));
+            });
+    };
+
     return (
         <div>
+            {delMessage && (
+                <h1
+                    onClick={() => {
+                        setIsHidden(true);
+                    }}
+                    className={`text-red-500 text-center p-5 cursor-pointer ${
+                        isHidden ? "hidden" : ""
+                    } `}
+                >
+                    {delMessage}
+                </h1>
+            )}
             {songs.map((song) => (
                 <div
                     className="border border-slate-800 rounded-md py-2 px-6 m-3"
@@ -24,6 +48,12 @@ const View = () => {
                     <p>Title: {song.title}</p>
                     <p>Artist: {song.artist}</p>
                     <p>Album: {song.album}</p>
+                    <button
+                        onClick={() => deleteSong(song.id)}
+                        className="border border-red-600  py-2 my-2 px-4 font-bold hover:text-white rounded-xl hover:bg-red-600 transition"
+                    >
+                        X
+                    </button>
                 </div>
             ))}
             <Link to="/">

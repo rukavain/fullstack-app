efab7dbec53a102a9faf611420440617716d5fcc
johnivann <magtotojohnivan@gmail.com>
2024-01-16 21:05:24 +0800

    added basic button design on main menu

--- react/src/Pages/Home.jsx
+++ react/src/Pages/Home.jsx
@@ -6,16 +6,13 @@ const Home = () => {
         <>
             <nav>
                 <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
+                    <li className="border border-slate-800 px-6 py-2 rounded-md hover:bg-slate-800 hover:text-white text-center my-2 transition cursor-pointer">
                         <Link to="/create">Add</Link>
                     </li>
-                    <li>
+                    <li className="border border-slate-800 px-6 py-2 rounded-md hover:bg-slate-800 hover:text-white text-center my-2 transition cursor-pointer">
                         <Link to="/view">View</Link>
                     </li>
-                    <li>
+                    <li className="border border-slate-800 px-6 py-2 rounded-md hover:bg-slate-800 hover:text-white text-center my-2 transition cursor-pointer">
                         <Link to="/update">Update</Link>
                     </li>
                 </ul>
